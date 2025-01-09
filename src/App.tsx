71a32b94a6f8164001bbbdbb0878ecac8d09bdf0
Yevhenii Kucherenko <chelsea7smile@gmail.com>
2025-01-09 18:12:30 +0200

    Change structure after review, add aliases globally for the project, move content to shared folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-//import './App.scss';
-
-import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage'
+import {NotFoundPage} from './pages/NotFoundPage/NotFoundPage';
 
 export const App = () => (
-  <div className='App'>
-    <h1>The site is under development</h1>
-    <NotFoundPage />{' '}
-  </div>
-)
+	<div className='App'>
+		<NotFoundPage />
+	</div>
+);
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import NotFound from '../../assets/images/page_not_found/page-not-found.png'
-import '../../styles.scss'
+import React from 'react';
+import NotFound from '@best-practice/images/pages/page-not-found.png';
+import '../../styles.scss';
 
 export const NotFoundPage: React.FC = () => {
-  return (
-    <div className='page-not-found-container'>
-      <h1 className='page-not-found-title'>Page not found</h1>
-      <div className='not-found-page-img'>
-        <img src={NotFound} className='not-found-page-img' alt='NotFoundPage' />
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='page-not-found-container'>
+			<h1 className='page-not-found-title'>Page not found</h1>
+			<div className='not-found-page-img'>
+				<img src={NotFound} className='not-found-page-img' alt='Page not found' />
+			</div>
+		</div>
+	);
+};
