6835c9e769885648ad721e708a0e0cb8167b1907
Your Name <vitaliy.savenets@gmail.com>
2025-01-10 21:51:17 +0200

    Fix incorrect image, move variables to a separate file and refactoring

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,32 +8,24 @@ export const Footer = () => {
 	};
 
 	return (
-		<>
-			<div className={styles.container_for_testing}>
-				{' '}
-				{/* Для тестування*/}
-				block for testing
-			</div>
-
-			<footer className={styles.footer}>
-				<div className={styles.footer_wrapper}>
-					<a className={styles.wrapper_item} href='https://example.com'>
-						<img src={logo} alt='logo' />
-					</a>
-					<div className={`${styles.wrapper_item} ${styles.navbar}`}>
-						<a href='https://example.com'>Github</a>
-						<a href='https://example.com'>Contacts</a>
-						<a href='https://example.com'>rights</a>
-					</div>
-
-					<div className={`${styles.wrapper_item} ${styles.third_item}`}>
-						<span>Back to top</span>
-						<button onClick={scrollToTop} className={styles.back_to_top_button}>
-							<img src={back_to_top_icon} alt='back to top icon' />
-						</button>
-					</div>
+		<footer className={styles.footer}>
+			<div className={styles.footer_wrapper}>
+				<a className={styles.footer_item} href='https://example.com'>
+					<img src={logo} alt='logo' />
+				</a>
+				<div className={`${styles.footer_item} ${styles.footer_navbar}`}>
+					<a href='https://example.com'>Github</a>
+					<a href='https://example.com'>Contacts</a>
+					<a href='https://example.com'>rights</a>
 				</div>
-			</footer>
-		</>
+
+				<div className={`${styles.footer_item} ${styles.back_to_top}`}>
+					<span>Back to top</span>
+					<button onClick={scrollToTop} className={styles.back_to_top_button}>
+						<img src={back_to_top_icon} alt='back to top icon' />
+					</button>
+				</div>
+			</div>
+		</footer>
 	);
 };
