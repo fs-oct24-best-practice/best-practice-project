03ff349a20bf8899de958f33a06c46c8eaadaf94
Your Name <vitaliy.savenets@gmail.com>
2025-01-10 03:52:00 +0200

    Initial footer layout implemented

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,39 @@
-import './Footer.scss';
+import styles from './Footer.module.scss';
+import logo from './Logo.svg';
+import back_to_top_icon from './back-to-top-icon.svg';
 
 export const Footer = () => {
+	const scrollToTop = () => {
+		window.scrollTo({top: 0, behavior: 'smooth'});
+	};
+
 	return (
-		<div className='Footer'>
-			<div className='footerLogo'>AppleStore Footer</div>
-			<nav className='footerNav'>
-				<li>github</li>
-				<li>Contacts</li>
-				<li>Rights</li>
-			</nav>
-		</div>
+		<>
+			<div className={styles.container_for_testing}>
+				{' '}
+				{/* Для тестування*/}
+				block for testing
+			</div>
+
+			<footer className={styles.footer}>
+				<div className={styles.footer_wrapper}>
+					<a className={styles.wrapper_item} href='https://example.com'>
+						<img src={logo} alt='logo' />
+					</a>
+					<div className={`${styles.wrapper_item} ${styles.navbar}`}>
+						<a href='https://example.com'>Github</a>
+						<a href='https://example.com'>Contacts</a>
+						<a href='https://example.com'>rights</a>
+					</div>
+
+					<div className={`${styles.wrapper_item} ${styles.third_item}`}>
+						<span>Back to top</span>
+						<button onClick={scrollToTop} className={styles.back_to_top_button}>
+							<img src={back_to_top_icon} alt='back to top icon' />
+						</button>
+					</div>
+				</div>
+			</footer>
+		</>
 	);
 };
