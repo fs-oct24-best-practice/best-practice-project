39966701701dc61a13a40a6fc7fc45548c311e23
TargoniyAnatoliy <targoniyanatoliy@gmail.com>
2025-01-10 12:28:43 +0200

    add NavLink in Header, add changes after pr review

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,6 @@ import { NavLink } from 'react-router'
 import './Header.scss'
 
 export const Header = () => {
-  //const { pathname, key } = useLocation();
-
-  console.log(location)
-
   return (
     <div className="Header">
       <nav className="Header__nav">
