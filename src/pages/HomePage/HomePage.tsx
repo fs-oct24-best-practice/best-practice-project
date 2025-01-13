cb01bb7431e26b90329c2c651fbacb052cad1f6e
TargoniyAnatoliy <targoniyanatoliy@gmail.com>
2025-01-13 02:26:04 +0200

    add home page layout (banner section, new models, hot prices), install swiper

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,54 @@
+import { useEffect, useState } from 'react';
+import { Product } from '../../types/Product';
+import { getProducts } from '../../components/api/apiE';
+import { Loader } from '../../components/Loader';
+import { Carousel } from '../../components/Carousel';
+
+import './HomePage.scss';
+import { Slider } from '../../components/Slider';
+
 export const HomePage = () => {
-	return <h1>Home Page</h1>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    getProducts()
+      .then((data) => setProducts(data))
+      .catch(() => 'Unable to load data from server!')
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+      });
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div className='home-page'>
+      <div className='home-page__container'>
+        <div className='home-page__title'>
+          <h1>Welcome to Nice Gadgets store!</h1>
+        </div>
+
+        <Carousel />
+
+        <section className='new-models'>
+          <Slider products={products} title='Brand new models' />
+        </section>
+
+        <section>
+          <h3>Shop by Categories</h3>
+        </section>
+
+        <section className='hot-prices'>
+          <Slider products={products} title='Hot Prices' />
+        </section>
+      </div>
+    </div>
+  );
 };
