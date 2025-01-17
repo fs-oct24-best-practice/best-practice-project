32f2b4bcefca43b7537af48e1e596a98608354c6
Yevhenii Kucherenko <chelsea7smile@gmail.com>
2025-01-17 23:21:22 +0200

    fix styles

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Catalog } from '../../components/Catalog/Catalog';
-import { getProductList } from '../../_public/api/api';
-import { ProductCategories } from '../../utils/ProductCategories';
-import { Product } from '../../types/Product';
+// import React from 'react';
+// import { Catalog } from '../../components/Catalog/Catalog';
+// import { getProductList } from '../../_public/api/api';
+// import { ProductCategories } from '../../utils/ProductCategories';
+// import { Product } from '../../types/Product';
 
-type CategoryProps = {
-  category: ProductCategories;
-};
+// type CategoryProps = {
+//   category: ProductCategories;
+// };
 
-export const CatalogPage: React.FC<CategoryProps> = ({ category }) => {
-  const fetchProducts = async (): Promise<Product[]> => {
-    const items = await getProductList(category);
+// export const CatalogPage: React.FC<CategoryProps> = ({ category }) => {
+//   const fetchProducts = async (): Promise<Product[]> => {
+//     const items = await getProductList(category);
 
-    console.log(items);
+//     console.log(items);
 
-    return items.map((item) => ({
-      ...item,
-      itemId: item.id,
-      fullPrice: item.priceRegular,
-      year: item.year ?? 2025,
-    }));
-  };
+//     return items.map((item) => ({
+//       ...item,
+//       itemId: item.id,
+//       fullPrice: item.priceRegular,
+//       year: item.year ?? 2025,
+//     }));
+//   };
 
-  return (
-    <Catalog
-      fetchProducts={fetchProducts}
-      title={category.charAt(0).toUpperCase() + category.slice(1)}
-    />
-  );
-};
+//   return (
+//     <Catalog
+//       fetchProducts={fetchProducts}
+//       title={category.charAt(0).toUpperCase() + category.slice(1)}
+//     />
+//   );
+// };
